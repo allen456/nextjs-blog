b45e44ec2cb61be12f3dc89661a77790afdefed2
allen456 <alen456@pm.me>
2023-01-30 02:52:03 +0800

    removed extra code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-    res.status(200).json({ method: req.method, path: req.path, host: req.host, protocol: req.protocol, text: 'Hello' });
+    res.status(200).json({ method: req.method, text: 'Hello' });
 }
